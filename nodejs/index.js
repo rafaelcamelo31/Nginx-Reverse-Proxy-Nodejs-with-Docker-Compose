fc3a90f4bdacb40e7601603119b32304ce113239
Rafael <rafaelcamelo31@gmail.com>
2021-02-25 23:58:18 -0400

    Initial assignment commit

---
+++ nodejs/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Rafael')`
+const select = `SELECT * FROM  people`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
+    res.send(
+        '<h1>Full Cycle</h1>'
+            `${select}`
+    )
+})
+
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`)
+})
